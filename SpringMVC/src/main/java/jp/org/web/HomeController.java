d820e58405a0e3ccd8103d27e1de59f042c44bf4
Mockingbird <partir_0618@yahoo.co.jp>
2018-10-22 00:08:44 +0900

    仕様理解のためコメント作成(途中)

--- SpringMVC/src/main/java/jp/org/web/HomeController.java
+++ SpringMVC/src/main/java/jp/org/web/HomeController.java
@@ -20,15 +20,21 @@ public class HomeController {
 	
 	// ロガーを取得するインスタンスの作成
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	// valueで指定したURLに対してRequestMethodを実行する
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	
 	/**
 	 * 
+	 * @param locale Localeクラスの引数
+	 * @param model　Modelクラスの引数
+	 * @return　homeメソッドを返す
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		// Date型の定義
 		Date date = new Date();
+		// 日付／時刻をlong形式に変換
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
